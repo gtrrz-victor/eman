23bac65b85108a51b8d8ce8e38c816e964c7c0d6
Victor <gtrrz.victor@gmail.com>
2023-09-25 12:24:09 +1000

    remove not needed attribute

--- apps/cadmin/app/events/manage-guest/page.tsx
+++ apps/cadmin/app/events/manage-guest/page.tsx
@@ -4,5 +4,5 @@ import { fetchPeople } from "@/lib/people"
 export default async function ManageGuest() {
     const people = await fetchPeople({ devMode: process.env.NODE_ENV === 'development' })
 
-    return <PeopleTableView guests={people.Items || []} suppressHydrationWarning />
+    return <PeopleTableView guests={people.Items || []} />
 }
