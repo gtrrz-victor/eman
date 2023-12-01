71f459d9eaf305679c0007abba3a3b83f3855837
Victor <gtrrz.victor@gmail.com>
2023-12-01 18:17:54 +1100

    allow invalidate data after sending notification

--- apps/cadmin/app/events/manage-guest/page.tsx
+++ apps/cadmin/app/events/manage-guest/page.tsx
@@ -2,7 +2,9 @@ import PeopleTableView from "@/components/people-table-view"
 import { fetchPeople } from "@/lib/people"
 
 export default async function ManageGuest() {
-    const people = await fetchPeople({ devMode: process.env.NODE_ENV === 'development' })
+    let people = await fetchPeople({ devMode: process.env.NODE_ENV === 'development' })
 
-    return <PeopleTableView guests={people.Items || []} />
+    return <PeopleTableView guests={people.Items || []} invalidate={async () => {
+        people = await fetchPeople({ devMode: process.env.NODE_ENV === 'development' })
+    }} />
 }
